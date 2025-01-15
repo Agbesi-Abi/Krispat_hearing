087075f27e43ba62e95d11dd4fb0e69ffe609e68
Agbesi-Abi <agbesia55@gmail.com>
2025-01-15 16:18:25 +0000

    first commit

--- src/component/HeroSection.jsx
+++ src/component/HeroSection.jsx
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from "react";
 import "../assets/css/HeroSection.css";
+import hero1 from "../assets/images/hero-1.jpg";
+import hero2 from "../assets/images/family-1.png";
+import hero3 from "../assets/images/hero-2.jpg";
+import hero4 from "../assets/images/male-audiologist-.jpg";
 
 function HeroSection() {
   const [currentImage, setCurrentImage] = useState(0);
-  const images = [
-    "/src/assets/images/hero-1.jpg",
-    "/src/assets/images/family-1.png",
-    "/src/assets/images/hero-2.jpg",
-    "/src/assets/images/male-audiologist-.jpg",
-  ];
+  const images = [hero1, hero2, hero3, hero4];
 
   // Auto-slide effect
   useEffect(() => {
@@ -31,14 +30,14 @@ function HeroSection() {
   return (
     <div className="hero-section relative inset-0 z-0">
       {/* Overlay */}
-      <div className="absolute inset-0 bg-black opacity-25  z-10"></div>
+      <div className="absolute inset-0 bg-black opacity-25 z-10"></div>
 
       {/* Image Slider */}
       <div className="slider relative">
         {/* Image */}
         <img
           src={images[currentImage]}
-          alt="Hero "
+          alt="Hero"
           className="hero-image w-full sm:h-[90vh] object-cover mt-20"
         />
       </div>
