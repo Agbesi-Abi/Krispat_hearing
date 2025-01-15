0976a67e284f3a1698e0c566e21bd1039af1992d
Agbesi-Abi <agbesia55@gmail.com>
2025-01-15 16:04:10 +0000

    first commit

--- src/component/HeroSection.jsx
+++ src/component/HeroSection.jsx
@@ -31,7 +31,7 @@ function HeroSection() {
   return (
     <div className="hero-section relative inset-0 z-0">
       {/* Overlay */}
-      <div className="absolute inset-0 bg-black opacity-25 z-10"></div>
+      <div className="absolute inset-0 bg-black opacity-25  z-10"></div>
 
       {/* Image Slider */}
       <div className="slider relative">
@@ -46,7 +46,7 @@ function HeroSection() {
       {/* Next and Previous Buttons */}
       <button
         onClick={handlePrevious}
-        className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full shadow-lg hover:bg-gray-600 focus:outline-none z-20"
+        className="absolute top-1/2 left-4 -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full shadow-lg hover:bg-gray-600 focus:outline-none z-20"
         aria-label="Previous Image"
       >
         &larr;
